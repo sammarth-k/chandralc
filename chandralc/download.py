2d018d00c608f68cf26e760281e2a7e13bb1b47b
Sammarth <kumarsammarth@gmail.com>
2021-07-16 12:28:47 +0530

    3 more galaxies

--- chandralc/download.py
+++ chandralc/download.py
@@ -12,7 +12,7 @@ from chandralc import convert
 # Downloading databases
 
 # list of galaxies with extracted lightcurves
-dbs = ["M101", "M104", "M81", "M84", "M74", "M51", "NGC1399", "M87"]
+dbs = ["M101", "M104", "M81", "M84", "M74", "M51", "NGC1399", "NGC5128", "M87", "M83", "M49"]
 
 
 def download_db():
